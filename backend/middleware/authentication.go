07bba39f69c356fa52f966d5a526846f9c6f15e1
itsgitz <anggit.ginanjar@outlook.com>
2022-08-16 21:18:27 +0700

    feat(middleware): Updated middleware, added verify and login authentication. Use handler struct.

--- backend/middleware/authentication.go
+++ backend/middleware/authentication.go
@@ -1,17 +1,64 @@
 package middleware
 
 import (
-	"log"
+	"errors"
+	"go-api/utils"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/session"
 )
+
+type Middleware struct {
+	Session *session.Store
+}
+
+func New(handlerSession *session.Store) *Middleware {
+	m := &Middleware{}
+	m.Session = handlerSession
+
+	return m
+}
 
 // NOTE: Fiber doesn't specifically define middleware types.
 // All we need to todo is create a simple handler but define it
 // on `app.Use(yourmiddleware)` method.
 
-// AuthenticationMiddleware middleware for authentication
-func AuthenticationMiddleware(c *fiber.Ctx) error {
-	log.Println("auth_middleware")
+// VerifyAuthentication middleware for authentication
+func (m *Middleware) VerifyAuthentication(c *fiber.Ctx) error {
+	userSession := getSession(c, m.Session)
+
+	if userSession == nil {
+		return c.JSON(utils.ErrorResponse(
+			errors.New("Invalid user session"),
+			"The user is not authenticated",
+		))
+	}
+
 	return c.Next()
+}
+
+func (m *Middleware) Authentication(c *fiber.Ctx) error {
+	userSession := getSession(c, m.Session)
+
+	uri := string(c.Request().URI().Path())
+
+	if uri != "/auth/logout" {
+		if userSession != nil {
+			return c.JSON(utils.ErrorResponse(
+				errors.New("Unable to authenticate user"),
+				"User is already logged in",
+			))
+		}
+	}
+
+	return c.Next()
+}
+
+func getSession(c *fiber.Ctx, store *session.Store) interface{} {
+	sess, err := store.Get(c)
+	if err != nil {
+		return nil
+	}
+
+	return sess.Get("username")
 }
