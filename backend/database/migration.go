3e0f1f6527f18db0c534db087bd3a447af421e9f
itsgitz <anggit.ginanjar@outlook.com>
2022-08-16 21:17:36 +0700

    feat(database): Added notes migration table

--- backend/database/migration.go
+++ backend/database/migration.go
@@ -6,12 +6,13 @@ import (
 	"time"
 )
 
-func InitDatabase() {
-	RunMigration()
+func (d *Database) InitDatabase() {
+	d.runUserMigration()
+	d.runNotesMigration()
 }
 
-func RunMigration() {
-	db, err := DatabaseConnection()
+func (d *Database) runUserMigration() {
+	db, err := d.Connection()
 	if err != nil {
 		panic(err)
 	}
@@ -39,5 +40,38 @@ func RunMigration() {
 
 	_ = rows
 
-	log.Println("Database table migrated!")
+	log.Println("Database table `users` migrated!")
+}
+
+func (d *Database) runNotesMigration() {
+	db, err := d.Connection()
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	ctx, cancel := context.WithTimeout(context.TODO(), 60*time.Millisecond)
+	defer cancel()
+
+	query := `
+	CREATE TABLE IF NOT EXISTS notes (
+		id serial PRIMARY KEY,
+		content TEXT NOT NULL,
+		created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+	)
+	`
+
+	result, err := db.ExecContext(ctx, query)
+	if err != nil {
+		panic(err)
+	}
+
+	rows, err := result.RowsAffected()
+	if err != nil {
+		panic(err)
+	}
+
+	_ = rows
+
+	log.Println("Database table `notes` migrated!")
 }
